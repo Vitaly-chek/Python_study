6aa8cb7c0c768e1d469a97cc121e828bf50ee8e0
Vitaly <krivoruchek1@gmail.com>
2023-01-13 14:09:00 +0200

    update file Less_1

---
+++ Ksendzov_course/Less_1.py
@@ -0,0 +1,23 @@
+# print("Hello")
+
+item_1 = 1
+item_0 = 0
+
+name = 'Vitalii'
+item_list = ['11', 22]
+item_list_e = []
+
+empty_item = 0
+
+b_item_T = True
+b_item_F = False
+
+
+if item_1:
+    print ('True item')
+else:
+    print ("False item")
+
+
+if name == "Vitalii":
+    print("Yes")
